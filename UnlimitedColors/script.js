adc6df0126a1d5363756e8696c0d609cea73d5d2
Kushagra Rastogi <kushagrarastogi20004@gmail.com>
2024-09-02 21:46:43 +0530

    js 4th project

---
+++ UnlimitedColors/script.js
@@ -0,0 +1,38 @@
+const start = document.querySelector('#start')
+const stopColor = document.querySelector('#stop')
+const body = document.querySelector('body')
+
+const randomColor = function(){
+    const hex = "0123456789abcdef";
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)]
+    }
+    return color
+}
+console.log(randomColor());
+
+let intervalId;
+
+let startChangingColor = function() {
+   if(!intervalId){
+    intervalId =  setInterval(changeBgColor,1000)
+   }
+
+    function changeBgColor() {
+        body.style.backgroundColor = randomColor()
+        // IF YOU WANT COLOR CODES
+        // console.log(randomColor());
+    }
+}
+
+let stopChangingColor = function() {
+    clearInterval(intervalId)
+    intervalId = null;
+};
+
+start.addEventListener('click', startChangingColor)
+
+stopColor.addEventListener('click',stopChangingColor)
+
+
